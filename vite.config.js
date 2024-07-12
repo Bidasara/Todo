07bd091ec44ef759de8492a1ee552402cfffcf2c
Bidasara <122274465+Bidasara@users.noreply.github.com>
2024-07-12 15:05:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5000,
+    host: '0.0.0.0',
+    strictPort: true,
+    open: false,
+    hmr: {
+      host: 'localhost',
+      port: 5000,
+    },
+  },
+});
+
+
